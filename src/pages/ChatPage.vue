d1ad5f2ee162c1fb437d7624436ef2313f54ed4b
nik_nik <nikbox88@gmail.com>
2023-02-03 18:18:41 +0300

    + message in tmplte

--- src/pages/ChatPage.vue
+++ src/pages/ChatPage.vue
@@ -1,13 +1,35 @@
 <template lang="pug">
-q-page(class="flex flex-center")
+q-page(class="flex column")
+  div(class="q-pa-md column col justify-end")
+    q-chat-message(
+      name="me"
+      :text="['hey, how are you?']"
+      sent
+      )
+    q-chat-message(
+      name="Jane"
+      :text="['doing fine, how r you?']"
+      )
+
+
+
   q-footer(elevated)
     q-toolbar
-      q-input(outlined v-model="text" label="Let's Write" bg-color="white" rounded dense)
-        template(v-slot:append)
-          q-icon(v-if="text !== ''" name="close" @click="text = ''" class="cursor-pointer")
-          q-icon(name="schedule")
+      q-input(
+        outlined
+        label="Let's whrite ASS !"
+        bg-color="white"
+        rounded
+        dense
+        v-model="newMessage"
+        ).full-width
         template(v-slot:after)
-          <q-btn color="white" round dense flat icon="send" />
+          q-btn(
+            color="white"
+            round
+            dense
+            flat
+            icon="send")
 </template>
 
 <script>
@@ -15,5 +37,8 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "ChatPage",
+  data: () => {
+    newMessage: "";
+  },
 });
 </script>
