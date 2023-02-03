0f9167336094925bf86241e99985465051944389
nik_nik <nikbox88@gmail.com>
2023-02-03 16:13:11 +0300

    first commit

---
+++ src/components/ChatList.vue
@@ -0,0 +1,45 @@
+<template lang="pug">
+q-item(
+  clickable
+  tag="a"
+  target="_blank"
+  )
+  q-item-section(
+    v-if="icon"
+    avatar
+    )
+    q-icon(:name="icon")
+
+  q-item-section
+    q-item-label {{ title }}
+    q-item-label(caption) {{ caption }}
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ChatList",
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+
+    caption: {
+      type: String,
+      default: "",
+    },
+
+    link: {
+      type: String,
+      default: "#",
+    },
+
+    icon: {
+      type: String,
+      default: "",
+    },
+  },
+});
+</script>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,95 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
+
+        <q-toolbar-title> ChatQF </q-toolbar-title>
+
+        <div>💬</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+      <q-list>
+        <q-item-label header> USER 1 </q-item-label>
+
+        <ChatList v-for="link in EssentialLinks" :key="link.title" v-bind="link" />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+import ChatList from "components/ChatList.vue";
+
+const linksList = [
+  // {
+  //   title: "Docs",
+  //   caption: "quasar.dev",
+  //   icon: "school",
+  //   link: "https://quasar.dev",
+  // },
+  // {
+  //   title: "Github",
+  //   caption: "github.com/quasarframework",
+  //   icon: "code",
+  //   link: "https://github.com/quasarframework",
+  // },
+  // {
+  //   title: "Discord Chat Channel",
+  //   caption: "chat.quasar.dev",
+  //   icon: "chat",
+  //   link: "https://chat.quasar.dev",
+  // },
+  {
+    title: "User 2",
+    caption: "Я в теме",
+    icon: "record_voice_over",
+    link: "https://forum.quasar.dev",
+  },
+  // {
+  //   title: "Twitter",
+  //   caption: "@quasarframework",
+  //   icon: "rss_feed",
+  //   link: "https://twitter.quasar.dev",
+  // },
+  // {
+  //   title: "Facebook",
+  //   caption: "@QuasarFramework",
+  //   icon: "public",
+  //   link: "https://facebook.quasar.dev",
+  // },
+  // {
+  //   title: "Quasar Awesome",
+  //   caption: "Community Quasar projects",
+  //   icon: "favorite",
+  //   link: "https://awesome.quasar.dev",
+  // },
+];
+
+export default defineComponent({
+  name: "MainLayout",
+
+  components: {
+    ChatList,
+  },
+
+  setup() {
+    const leftDrawerOpen = ref(false);
+
+    return {
+      EssentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+    };
+  },
+});
+</script>
