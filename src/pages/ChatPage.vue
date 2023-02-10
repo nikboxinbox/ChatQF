67e9906c3e0c0b08835439c0fb57324e7af59dce
nik_nik <nikbox88@gmail.com>
2023-02-10 16:01:29 +0300

    + ts & TODO

--- src/pages/ChatPage.vue
+++ src/pages/ChatPage.vue
@@ -2,43 +2,62 @@
 q-page(class="flex column")
   div(class="q-pa-md column col justify-end")
     q-chat-message(
-      name="me"
-      :text="['hey, how are you?']"
-      sent
+      v-for="message in messages"
+      :key="message.text"
+      :name="message.from"
+      :text="[message.text]"
+      :sent="message .from === 'Me'"
       )
-    q-chat-message(
-      name="Jane"
-      :text="['doing fine, how r you?']"
-      )
+    //- q-chat-message(
+    //-   name="Jane"
+    //-   :text="[ass]"
+    //-   )
 
 
-
+    //- TODO: не срабатывает сабмит по нажатию кнопки
   q-footer(elevated)
     q-toolbar
-      q-input(
-        outlined
-        label="Let's whrite ASS !"
-        bg-color="white"
-        rounded
-        dense
-        v-model="newMessage"
-        ).full-width
-        template(v-slot:after)
-          q-btn(
-            color="white"
-            round
-            dense
-            flat
-            icon="send")
+      q-form(@submit="sendMessage").full-width
+        q-input(
+          outlined
+          label="Let's whrite ASS !"
+          bg-color="white"
+          rounded
+          dense
+          v-model="newMessage"
+          )
+          template(v-slot:after)
+            q-btn(
+              label="Submit"
+              type="submit"
+              color="white"
+              round
+              dense
+              flat
+              icon="send")
 </template>
 
-<script>
+<script lang="ts">
 import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "ChatPage",
-  data: () => {
-    newMessage: "";
+  data: () => ({
+    newMessage: "",
+    messages: [
+      { text: "Hi", from: "Mike" },
+      { text: "Hi Mike", from: "Me" },
+      { text: "How are you", from: "Mike" },
+    ],
+  }),
+  methods: {
+    sendMessage() {
+      debugger;
+      this.messages.push({
+        text: this.newMessage,
+        from: "me",
+      });
+    },
   },
 });
 </script>
